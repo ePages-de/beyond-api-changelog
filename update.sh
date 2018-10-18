148696d426baac16d4bc31ebce097d05293dc178
Jan Mewes <jan.mewes@epages.com>
2018-10-18 14:12:20 +0200

    Revert to old API spec

--- update.sh
+++ update.sh
@@ -7,6 +7,9 @@ API_SPEC_URL='http://docs.beyondshop.cloud/openapi.yaml'
 API_SPEC_FILE=openapi.yaml
 CHANGE_LOG_FILE=beyond-api-changelog.md
 
+PREVIOUS_API_SPEC_FILE=$(mktemp)
+PREVIOUS_API_SPEC_COMMIT=$(git log -1 --pretty=format:"%h" -- ${API_SPEC_FILE})
+
 curl -s ${API_SPEC_URL} > ${API_SPEC_FILE}
 git diff --exit-code > /dev/null
 if [[ $? -eq 0 ]]; then
@@ -20,9 +23,6 @@ git commit -m "Update API spec"
 ################################################################################
 # Update change log
 ################################################################################
-PREVIOUS_API_SPEC_FILE=$(mktemp)
-PREVIOUS_API_SPEC_COMMIT=$(git log -1 --pretty=format:"%h" -- ${API_SPEC_FILE})
-
 git show ${PREVIOUS_API_SPEC_COMMIT}:${API_SPEC_FILE} > ${PREVIOUS_API_SPEC_FILE}
 ./prepend-diff-description.sh -o ${PREVIOUS_API_SPEC_FILE} -n ${API_SPEC_FILE} -c ${CHANGE_LOG_FILE}
 
