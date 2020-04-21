fcb454a08ca766e79af2a26e4e1310e70484199e
Ioannis Koutras <ikoutras@epages.com>
2020-04-21 17:18:12 +0200

    send-notification.sh: Use Mailgun EU

--- send-notification.sh
+++ send-notification.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 curl --user "api:${MAILGUN_API_TOKEN}" \
-  https://api.mailgun.net/v3/${MAILBOX}/messages \
+  https://api.eu.mailgun.net/v3/${MAILBOX}/messages \
   -F subject="Beyond API Changelog" \
   -F from="noreply@epages.com" \
   -F html="<-" "$@"
