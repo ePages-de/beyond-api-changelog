18982434a255fce23bacf65b959f129780b3bcb7
Ioannis Koutras <ikoutras@epages.com>
2020-04-21 17:17:40 +0200

    Sanitize update.sh

--- update.sh
+++ update.sh
@@ -12,8 +12,8 @@ PREVIOUS_API_SPEC_FILE=$(mktemp)
 PREVIOUS_API_SPEC_COMMIT=$(git log -1 --pretty=format:"%h" -- ${CHANGE_LOG_FILE})
 
 curl -s ${API_SPEC_URL} > ${API_SPEC_FILE}
-git diff --exit-code > /dev/null
-if [[ $? -eq 0 ]]; then
+
+if git diff --exit-code > /dev/null; then
     echo "API spec already up to date."
     exit 0
 fi
@@ -21,9 +21,9 @@ fi
 ################################################################################
 # Update change log
 ################################################################################
-git show ${PREVIOUS_API_SPEC_COMMIT}:${API_SPEC_FILE} > ${PREVIOUS_API_SPEC_FILE}
+git show "${PREVIOUS_API_SPEC_COMMIT}:${API_SPEC_FILE}" > "${PREVIOUS_API_SPEC_FILE}"
 EMAIL_BODY_FILE=$(mktemp)
-./prepend-diff-description.sh -o ${PREVIOUS_API_SPEC_FILE} -n ${API_SPEC_FILE} -c ${CHANGE_LOG_FILE} -e ${EMAIL_BODY_FILE}
+./prepend-diff-description.sh -o "${PREVIOUS_API_SPEC_FILE}" -n "${API_SPEC_FILE}" -c "${CHANGE_LOG_FILE}" -e "${EMAIL_BODY_FILE}"
 
 git diff --exit-code --name-only ${CHANGE_LOG_FILE} > /dev/null
 if [[ $? -eq 1 ]]; then
@@ -32,7 +32,7 @@ if [[ $? -eq 1 ]]; then
 
     if [[ -s ${EMAIL_BODY_FILE} ]] && [[ -n ${MAILGUN_API_TOKEN} ]] && [[ -n ${MAILBOX} ]] ; then
       echo "Sending out email with changelog"
-      cat ${EMAIL_BODY_FILE} | ./send-notification.sh -F to="jan.mewes@experimental-software.com"
+      ./send-notification.sh -F to="jan.mewes@experimental-software.com" < "${EMAIL_BODY_FILE}"
     fi
 fi
 
