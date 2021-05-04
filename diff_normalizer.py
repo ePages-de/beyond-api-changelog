5cffebb886df3faa45923a6a6d88658cc0668321
Jan Mewes <jan.mewes@epages.com>
2021-05-04 13:44:47 +0200

    WIP

--- diff_normalizer.py
+++ diff_normalizer.py
@@ -1,5 +1,36 @@
 import sys
 
+#######
+# Parse raw diff
+#######
+sections = []
+
+currentSection = None
+currentChangeText = None
+
 for line in sys.stdin:
-    print("hello")
-    print(line)
+
+    if (line.startswith("###")):
+        currentSection = {}
+        currentSection["headline"] = line
+        currentSection["changes"] = []
+        sections.append(currentSection)
+        continue
+
+    if (line.startswith("*")):
+        currentChangeText = line
+        currentSection["changes"].append(currentChangeText)
+        continue
+
+    if (currentChangeText):
+        currentChangeText += line
+
+
+#######
+# Print normalized diff
+#######
+
+# for section in sections:
+#     print(section["headline"])
+#     for change in section["changes"]:
+#         print(change)
